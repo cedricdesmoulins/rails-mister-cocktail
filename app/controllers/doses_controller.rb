43995c98148661b18d903e079b28baa4abd1e4bb
cedric desmoulins <cdesmoulins@euromotion.care>
2018-08-10 11:33:49 +0200

    day 1

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,32 @@
 class DosesController < ApplicationController
+
+  def create
+    @cocktail = Cocktail.find(params[:garden_id])
+    @dose = @cocktail.doses.new(doses_params)
+    if @doses.save
+      redirect_to @cocktail
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:name)
+  end
+
 end
+
+
+
+
+
+
